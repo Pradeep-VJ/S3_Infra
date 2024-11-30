8d6b7c5b13a2baacf3e85670c4f82c80beeff834
Pradeep Reddy B <66411998+Pradeep-VJ@users.noreply.github.com>
2024-11-30 18:47:12 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,7 @@
 provider "aws" {
   region = "us-east-1" # Specify your AWS region
 }
-variable "lifecycle_policy_file_url" {
-  default = "https://github.com/Pradeep-VJ/S3_Infra/blob/main/qa-inbound-lcrs.json"
-}
+
 resource "aws_s3_bucket" "inbound_s3" {
   bucket = "inbound-astra-files"
   tags = {
@@ -13,10 +11,14 @@ resource "aws_s3_bucket" "inbound_s3" {
     PIIData      = "true"
     CreditData   = "false"
   }
-
-  # Referencing the lifecycle rules JSON file directly
   lifecycle_rule {
-    enabled = true
-    source = var.lifecycle_policy_file_url
+    enabled = false
+    id      = "TransitionToDeepArchive"
+    prefix  = ""
+    transition {
+      days          = 21
+      storage_class = "GLACIER_DEEP_ARCHIVE"
+    }
   }
 }
+
