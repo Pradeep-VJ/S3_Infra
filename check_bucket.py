893087f6a2b5c5f59ddbdbe61ce792a6c78dc41e
Pradeep Reddy B <66411998+Pradeep-VJ@users.noreply.github.com>
2024-11-30 22:40:34 -0500

    Update check_bucket.py

--- check_bucket.py
+++ check_bucket.py
@@ -1,11 +1,12 @@
 import boto3
 import sys
+import json
 
 bucket_name = sys.argv[1]
 s3 = boto3.client('s3')
 
 try:
     s3.head_bucket(Bucket=bucket_name)
-    print(f'{{"exists": true}}')
-except:
-    print(f'{{"exists": false}}')
+    print(json.dumps({"exists": "true"}))  # Output as string
+except Exception:
+    print(json.dumps({"exists": "false"}))  # Output as string
