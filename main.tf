de9ae304f9621c32f353ac0c40d6549f48254650
Pradeep Reddy B <66411998+Pradeep-VJ@users.noreply.github.com>
2024-11-30 19:30:55 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,22 +2,52 @@ provider "aws" {
   region = "us-east-1" # Change to your desired region
 }
 
-resource "aws_s3_bucket" "inbound_s3" {
+resource "aws_s3_bucket" {
   bucket = "inbound-astra-files"
   tags = {
     Owner       = "Pradeep_Reddy_B"
     Environment = "QA"
     MedicalData = "false"
-    PIIData     = "true"
+    PIIData     = "false"
     CreditData  = "false"
   }
 
-  lifecycle_rule {
+  lifecycle_rule = local.lifecycle_rule
+}
+
+resource "aws_s3_bucket" {
+  bucket = "restricted-astra-files"
+  tags = {
+    Owner       = "Pradeep_Reddy_B"
+    Environment = "QA"
+    MedicalData = "false"
+    PIIData     = "true"
+    CreditData  = "true"
+  }
+
+  lifecycle_rule = local.lifecycle_rule
+}
+
+# Common lifecycle rule
+variable "common_lifecycle_rule" {
+  default = {
     enabled = true
-    id      = "lifecycle-rule"
+    id      = "Expire-in-30days"
     prefix  = ""
-    expiration {
-      days = 90
+    expiration = {
+      days = 30
     }
   }
+}
+
+# Function to apply the common lifecycle rule
+locals {
+  lifecycle_rule = [
+    {
+      enabled    = var.common_lifecycle_rule["enabled"]
+      id         = var.common_lifecycle_rule["id"]
+      prefix     = var.common_lifecycle_rule["prefix"]
+      expiration = var.common_lifecycle_rule["expiration"]
+    }
+  ]
 }
