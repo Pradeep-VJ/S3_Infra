1df57078b99b69829cb38080b053bd6be130c371
Pradeep Reddy B <66411998+Pradeep-VJ@users.noreply.github.com>
2024-12-01 19:42:29 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,5 @@
 provider "aws" {
-  region     = "us-east-1" # Replace with your region
+  access_key = env("AWS_ACCESS_KEY_ID")
+  secret_key = env("AWS_SECRET_ACCESS_KEY")
+  region     = "us-east-1"
 }
