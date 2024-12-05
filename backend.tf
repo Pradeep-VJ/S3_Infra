6c7c37e5906bc74035d399b1c7adb024ad05f13d
Pradeep Reddy B <66411998+Pradeep-VJ@users.noreply.github.com>
2024-12-05 11:24:02 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 # Backend configuration for Terraform state
 terraform {
   backend "s3" {
-    bucket         = "terraform-statefile-astra"  # Name of the S3 bucket for state file
-    key            = "terraform/statefile.tfstate"  # Path within the S3 bucket
+    bucket         = "Astra-terraform-statefiles"  # Name of the S3 bucket for state file
+    key            = "s3_statefile/terraform.tfstate"  # Path within the S3 bucket
     region         = "us-east-1"                 # AWS region where the bucket is located
     encrypt        = true                        # Enable encryption for the state file
-    dynamodb_table = "terraform-lock-table"      # DynamoDB table for state locking
+    #dynamodb_table = "terraform-lock-table"      # DynamoDB table for state locking
   }
 }
